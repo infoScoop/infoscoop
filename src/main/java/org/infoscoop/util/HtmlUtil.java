39a6d793ee1cc7ef5ba78019c6cf895b036bb100
hrendoh <hrendoh@gmail.com>
2010-05-27 01:17:51 +0000

    Issue 20:	Copyright notices must be attached in all files
    Update issue 20
    Add copyright to java files.
    
    
    git-svn-id: https://infoscoop.googlecode.com/svn/trunk@253 d4e23b88-c782-11de-b6dc-25c59a28ecba

--- src/main/java/org/infoscoop/util/HtmlUtil.java
+++ src/main/java/org/infoscoop/util/HtmlUtil.java
@@ -1,3 +1,20 @@
+/* infoScoop OpenSource
+ * Copyright (C) 2010 Beacon IT Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ */
+
 /*
  * $Id: HtmlUtil.java,v 1.4 2009/02/17 08:44:08 hr-endoh Exp $
  *
