0beb06a9beb5719aa4892baeb931a956929137f4
atskimura <atskimura@gmail.com>
2010-10-20 09:52:39 +0000

    Issue 270:  	 User Management console -Fixed Area Update issue 270 #183 links to the service to retrieve users and groups in 4g context
    git-svn-id: https://infoscoop.googlecode.com/svn/branches/3.0@712 d4e23b88-c782-11de-b6dc-25c59a28ecba

--- src/main/java/org/infoscoop/manager/controller/UserController.java
+++ src/main/java/org/infoscoop/manager/controller/UserController.java
@@ -2,141 +2,101 @@ package org.infoscoop.manager.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.infoscoop.dao.GroupDAO;
 import org.infoscoop.dao.UserDAO;
 import org.infoscoop.dao.model.Group;
 import org.infoscoop.dao.model.User;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import sample.appsforyourdomain.AppsForYourDomainClient;
-
-import com.google.gdata.data.Link;
-import com.google.gdata.data.appsforyourdomain.EmailList;
-import com.google.gdata.data.appsforyourdomain.provisioning.EmailListEntry;
-import com.google.gdata.data.appsforyourdomain.provisioning.EmailListFeed;
-import com.google.gdata.data.appsforyourdomain.provisioning.EmailListRecipientEntry;
-import com.google.gdata.data.appsforyourdomain.provisioning.EmailListRecipientFeed;
-import com.google.gdata.data.appsforyourdomain.provisioning.UserEntry;
-import com.google.gdata.data.appsforyourdomain.provisioning.UserFeed;
-
 @Controller
 public class UserController {
-	@RequestMapping(method=RequestMethod.GET)
+	private static Log log = LogFactory.getLog(UserController.class);
+
+	@RequestMapping(method = RequestMethod.GET)
 	public void index(Model model) throws Exception {
 	}
 
-	@RequestMapping(method=RequestMethod.GET)
-	public void sync(Model model) throws Exception {
-		
-		String adminAddress = "xxx@infoscoop.org";
+	@RequestMapping(method = RequestMethod.GET)
+	public void sync(Model model, HttpServletRequest request) throws Exception {
 		String domain = "infoscoop.org";
-		String password = "xxx";
-		AppsForYourDomainClient client = new AppsForYourDomainClient(adminAddress, password, domain);
+
 		long start = System.currentTimeMillis();
-		String firstName = null;
-		int userCount = 0;
-		int pageSize = 100;
-		Link nextLink = null;
+
+		HttpClient http = new HttpClient();
+		GetMethod method = new GetMethod(
+				"http://localhost:8080/infoscoop4g/usergroup?domain=" + domain);
+		http.executeMethod(method);
+		if (method.getStatusCode() >= 300) {
+			// TODO handle error
+			throw new Exception("Failed to retrieve users and groups. "
+					+ method.getStatusCode() + " - " + method.getStatusText());
+		}
+
+		String response = method.getResponseBodyAsString();
+		JSONObject json = new JSONObject(response);
+
 		UserDAO userDAO = UserDAO.newInstance();
-		do {
-			UserFeed users = client.retrievePageOfUsers(firstName);
-
-			for (UserEntry entry : users.getEntries()) {
-				String name = entry.getTitle().getPlainText();
-				User user = userDAO.getByName(name);
-				if(user == null)user = new User();
-				String mail = name + "@" + domain;
-				user.setEmail(mail);
-				user.setName(name);
-				firstName = name;
-				userDAO.save(user);
-			}
-			nextLink = users.getNextLink();
-			pageSize = users.getEntries().size();
-			userCount += pageSize;
-			if(nextLink != null)userCount--;
-			
-		}while(nextLink != null);
-		
-		model.addAttribute("userCount", userCount);
-
-		int groupCount = 0;
-		firstName = null;
-		GroupDAO groupDAO = GroupDAO.newInstance();
-		do {
-			EmailListFeed groups = client.retrievePageOfEmailLists(firstName);
-			for (EmailListEntry entry : groups.getEntries()) {
-				String name = entry.getTitle().getPlainText();
-				EmailList list = entry.getEmailList();
-				Group group = groupDAO.getByName(name);
-				if(group == null) group = new Group();
-				group.setName(name);
-				EmailListRecipientFeed recipients = client.retrieveAllRecipients(name);
-				for(EmailListRecipientEntry recipient : recipients.getEntries()){
-					String userName = recipient.getTitle().getPlainText().split("@")[0];
-					User user = userDAO.getByName(userName);
-					if(user != null)
-						group.addToUsers(user);
-					else
-						System.out.println("User " + userName + " is missing.");
-				}
-				groupDAO.save(group);
-				firstName = name;
-			}
-			nextLink = groups.getNextLink();
-			pageSize = groups.getEntries().size();
-			groupCount += pageSize;
-			if(nextLink != null)userCount--;
-		}while(nextLink != null);
-		
-		model.addAttribute("groupCount", groupCount);
-		System.out.println(System.currentTimeMillis() - start);
-
-/*
-		URL feedUrl = new URL("http://www.google.com/m8/feeds/profiles/domain/beacon-it.co.jp/full");
-		ContactsService service = new ContactsService("Google-contactsExampleApp-3");
-		service.setUserCredentials("y_yoshida@beacon-it.co.jp", "z1x2c3v4");
-
-		Query myQuery = new Query(feedUrl);
-		myQuery.setMaxResults(300);
-		ContactFeed resultFeed = service.query(myQuery, ContactFeed.class);
-
-// -----
-		for (ContactEntry entry : resultFeed.getEntries()) {
-			User user = new User();
-			String[] id = entry.getId().split("/");
-			String mail = id[id.length-1]+"@"+id[id.length-3];
+		JSONArray usersJ = json.getJSONArray("users");
+		for (int i = 0; i < usersJ.length(); i++) {
+			JSONObject userJ = usersJ.getJSONObject(i);
+			String name = userJ.getString("login");
+			User user = userDAO.getByName(name);
+			if (user == null)
+				user = new User();
+			String mail = name + "@" + domain;
+			System.out.println(mail);
 			user.setEmail(mail);
-//			user.setEmail(entry.getEmailAddresses().get(0).getAddress());
-			user.setName(entry.getTitle().getPlainText());
-			UserDAO.newInstance().save(user);
+			user.setName(name);
+			userDAO.save(user);
 		}
-//------
-*/
+		model.addAttribute("userCount", usersJ.length());
 
-/*
-		int length = resultFeed.getEntries().size();
-		int max = 100;
-		for (int i = 0; i < length/max; i++) {
-			Query query = new Query(feedUrl);
-			query.setMaxResults(max);
-			query.setStartIndex(i*max+1);
+		GroupDAO groupDAO = GroupDAO.newInstance();
+		JSONArray groupsJ = json.getJSONArray("groups");
+		for (int i = 0; i < groupsJ.length(); i++) {
+			JSONObject groupJ = groupsJ.getJSONObject(i);
+			String name = groupJ.getString("groupId");
+			Group group = groupDAO.getByName(name);
+			if (group == null)
+				group = new Group();
+			group.setName(name);
 
-			ContactFeed feed = service.query(query, ContactFeed.class);
-			for (ContactEntry entry : feed.getEntries()) {
-				User user = new User();
-				String[] id = entry.getId().split("/");
-				String mail = id[id.length-1]+"@"+id[id.length-3];
-				user.setEmail(mail);
-				user.setName(entry.getTitle().getPlainText());
-				UserDAO.newInstance().save(user);
+			JSONArray membersJ = groupJ.getJSONArray("members");
+			for (int j = 0; j < membersJ.length(); j++) {
+				JSONObject memberJ = membersJ.getJSONObject(j);
+				String memberId = memberJ.getString("memberId");
+				if (memberId.equals("*")) {
+					List<User> allusers = userDAO.all();
+					for (User u : allusers) {
+						group.addToUsers(u);
+					}
+					break;
+				}
+				String userName = memberId.split("@")[0];
+				User user = userDAO.getByName(userName);
+				if (user != null)
+					group.addToUsers(user);
+				else
+					System.out.println("User " + userName + " is missing.");
 			}
-
+			groupDAO.save(group);
 		}
-*/
+		model.addAttribute("groupCount", groupsJ.length());
+
+		long time = System.currentTimeMillis() - start;
+		log.info(usersJ.length() + " users and " + groupsJ.length()
+				+ " groups have been synchronized. It took " + time
+				+ " millisecond.");
 	}
 }
