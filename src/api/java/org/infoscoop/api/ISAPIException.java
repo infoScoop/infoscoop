c86fb33b6f641463a2fdf0a9cfd0638df656870f
aknishiumi <ak.nishiumi@gmail.com>
2013-11-22 16:56:10 +0900

    #539 TabLayout API ( /admin/tabs )

---
+++ src/api/java/org/infoscoop/api/ISAPIException.java
@@ -0,0 +1,13 @@
+package org.infoscoop.api;
+
+public class ISAPIException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ISAPIException(String message) {
+		super(message);
+	}
+}
