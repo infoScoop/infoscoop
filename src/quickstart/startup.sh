780a31fab941aaf048d5d7b28c17e90ab721ef2e
mikami <mikami@p01434>
2014-01-27 17:13:40 +0900

    QuickStartのstartup/shutdown修正

--- src/quickstart/startup.sh
+++ src/quickstart/startup.sh
@@ -1,4 +1,4 @@
 SELF_DIR=`dirname $0`
-CATALINA_HOME=${SELF_DIR}/apache-tomcat-6.0.28
+CATALINA_HOME=${SELF_DIR}/apache-tomcat-7.0.34
 JAVA_OPTS="-Xms128m -Xmx256m -XX:PermSize=64m -XX:MaxPermSize=128m"
 ${CATALINA_HOME}/bin/startup.sh
