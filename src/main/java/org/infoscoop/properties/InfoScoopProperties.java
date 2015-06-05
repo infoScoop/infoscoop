20e9023ba61a1fbd276a89e1c3b82957993b0ff8
aknishiumi <ak.nishiumi@gmail.com>
2015-06-05 14:06:04 +0900

    #598 マルチテナント対応 プロパティ管理クラス

---
+++ src/main/java/org/infoscoop/properties/InfoScoopProperties.java
@@ -0,0 +1,48 @@
+/* infoScoop OpenSource
+ * Copyright (C) 2010 Beacon IT Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ */
+
+package org.infoscoop.properties;
+
+import java.util.ResourceBundle;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.infoscoop.admin.web.AuthenticationServlet;
+
+public class InfoScoopProperties {
+	private static Log logger = LogFactory.getLog(AuthenticationServlet.class);
+	private static InfoScoopProperties singleInstance = new InfoScoopProperties();
+	private boolean useMultitenantMode = false;
+	
+	private InfoScoopProperties() {
+		try {
+			ResourceBundle bundle = ResourceBundle.getBundle("infoscoop");
+			String useMultitenantModeStr = bundle.getString("useMultitenantMode");
+			this.useMultitenantMode = new Boolean(useMultitenantModeStr);
+		} catch (Exception ex) {
+			logger.error("Failed to load infoscoop.properties.", ex);
+		}
+	}
+
+	public static InfoScoopProperties getInstance() {
+		return singleInstance;
+	}
+
+	public boolean isUseMultitenantMode() {
+		return useMultitenantMode;
+	}
+}
