fac96ae0361506b98411614c1e982ac27eb1db46
aknishiumi <ak.nishiumi@gmail.com>
2018-09-03 17:56:21 +0900

    必要なコードを削除していたため復帰

--- src/main/java/org/infoscoop/dao/AccountDAO.java
+++ src/main/java/org/infoscoop/dao/AccountDAO.java
@@ -195,6 +195,7 @@ public class AccountDAO extends HibernateDaoSupport {
 	
 	public int selectCountByMap(Map condition) {
 		DetachedCriteria criteria = createSearchCriteria(condition);
+		criteria.setProjection(Projections.rowCount());
 		
 		List list = super.getHibernateTemplate().findByCriteria(criteria);
 		return (int)list.get(0);
